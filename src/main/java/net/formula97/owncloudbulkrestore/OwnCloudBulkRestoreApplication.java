4023fb06c5678d7ede67a5bcd83779bfaf21056e
HAJIME Fukuna <f97one@gmail.com>
2021-06-03 22:33:10 +0900

    プロジェクト作成直後を追加。

---
+++ src/main/java/net/formula97/owncloudbulkrestore/OwnCloudBulkRestoreApplication.java
@@ -0,0 +1,13 @@
+package net.formula97.owncloudbulkrestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnCloudBulkRestoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwnCloudBulkRestoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/formula97/owncloudbulkrestore/OwnCloudBulkRestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package net.formula97.owncloudbulkrestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnCloudBulkRestoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
